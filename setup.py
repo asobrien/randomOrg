f95d83c20d864316d770635fd5c4375e536602df
Anthony O'Brien <asobrien@gmail.com>
2012-12-09 00:03:31 -0500

    Added setup.py for v0.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+	
+setup(
+    name='randorg',
+    version='0.1',
+    description='Python interface to Random.org',
+    author='BFTM',
+    author_email='bftm@permil.org',
+    url='https://github.com/asobrien/randomOrg',
+    packages=['randorg']
+	)
