8a219475a6d6ba5fc56c6a29698ff323cef3731b
Anthony O'Brien <asobrien@gmail.com>
2012-12-08 23:32:16 -0500

    Cleaned up _rand_core.py; simplified `integers` by using `get_http`

--- randorg/_rand_core.py
+++ randorg/_rand_core.py
@@ -1,13 +1,18 @@
-# HTTP GET to interact with Random.org
+# CORE UTILITIES TO CREATE HTTP GET REQUESTS TO INTERACT WITH RANDOM.ORG
 
 import urllib
 import urllib2
 import os.path
 import numpy as np
 
-# CONFIG
-RANDOM_URL = 'http://www.random.org'
 
+### CONFIG ###
+RANDOM_URL = 'http://www.random.org'
+### END CONFIG ###
+
+
+
+### MAIN FUNCTIONS ###
 
 def integers(num, minimum, maximum, base=10):
     # TODO: Ensure numbers within bounds
@@ -29,18 +34,9 @@ def integers(num, minimum, maximum, base=10):
             'base':base, 
             'format':'plain',
             'rnd':'new'}
-    
-    url_opts = urllib.urlencode(opts)
-    full_url = os.path.join(RANDOM_URL, FUNC_TYPE) + '/?' + url_opts
-    data = urllib2.urlopen(full_url)
-    if data.code == 200: 
-        # Status is Good
-        arr = np.fromstring(data.read(), sep='\n', dtype='int')
-    else:
-        print "random.org returned server code: %s" % data.code
-    return arr
-
-
+    integers = get_http(RANDOM_URL, function, opts)
+    integers_arr = str_to_arr(intergers)
+    return integers_arr
 
 
 def sequence(minimum, maximum):
@@ -79,19 +75,21 @@ def string(num, length, digits=False, upper=True, lower=True, unique=False):
     return seq
 
 
+def quota(ip=None):
+    """Check your quota."""
+    # TODO: Add arbitrary user defined IP check
+    url = 'http://www.random.org/quota/?format=plain'
+    data = urllib2.urlopen(url)
+    credit = int(data.read().strip())
+    if data.code == 200:
+        return credit
+    else:
+        return "ERROR: Server responded with code %s" % data.code
 
-def convert(self):
-    # TODO: Add proper TypeError
-    if self == True:
-        self = 'on'
-        return self
-    elif self == False:
-        self = 'off'
-        return self
-    else: 
-        return "TypeERROR: not a boolean"
 
 
+
+### HELPER FUNCTIONS ###
 
 def get_http(base_url, function, opts):
     """HTTP request generator."""
@@ -108,82 +106,17 @@ def str_to_arr(string):
     """Converts string to numpy array."""
     arr = np.fromstring(string, sep='\n', dtype='int')
     return arr
-    
-
-def quota(ip=None):
-    """Check your quota."""
-    # TODO: Add arbitrary user defined IP check
-    url = 'http://www.random.org/quota/?format=plain'
-    data = urllib2.urlopen(url)
-    credit = int(data.read().strip())
-    if data.code == 200:
-        return credit
-    else:
-        return "ERROR: Server responded with code %s" % data.code
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+def convert(self):
+    # TODO: Add proper TypeError
+    if self == True:
+        self = 'on'
+        return self
+    elif self == False:
+        self = 'off'
+        return self
+    else: 
+        return "TypeERROR: not a boolean"
 
-
-# 
-# 
-# #################################
-# ####### START OF PROGRAM ########
-# #################################
-# 
-# quotaCheck = urllib.urlopen('http://www.random.org/quota/?format=plain').read()  # Check to see if you have bits remaining on the server
-# 
-# if quotaCheck > 0:
-#     getString='http://www.random.org/integers/?num=25&min=0&max=16&col=1&base=16&format=plain&rnd=new'
-#     randomData = urllib.urlopen(getString).read()
-#     print(randomData)
-#     print 
-#     print 'You have', quotaCheck.strip(), 'bits remaining on the server.'
-#     
-# else:
-#     print 'You have no remaing requests on the Random.org server; wait at least 10 minutes before making another request. It may take 24 hours for you to gain access the server again.'
-#     
-# 
-# sitePath = 'http://www.random.org/'
-# 
-# # Integer sequence from random.org
-# def randInt(num, min, max, base=10, format='plain', columns=1, dataType='new'):
-#     appPath = 'integers/?'
-#     params = ['num', str(num),
-#               'min', str(min),
-#               'max', str(max), 
-#               'base', str(base), 
-#               'format', format,
-#               'col', str(columns),
-#               'rnd', dataType]
-#     
-#     for i in range(1, len(params) + len(params)/2, 3):
-#         params.insert(i, '=')
-#     
-#     for i in range(3, len(params) + (len(params)/3-1), 4):
-#         params.insert(i, '&')
-#         
-#     
-#     params = "".join(params)    
-#     getString = sitePath + appPath + params
-#     randomData = urllib.urlopen(getString).read()
-#     print(getString)
-#     print(randomData)
-#     return randomData
-#     # TODO: learn more about "return" in a function
-# 
-# 
-# # TODO: create a function that simulates tossing a coin by calling on randInt(tosses, min=0, max=1), where HEADS is sum/tosses >= 0.5.
-# 
-# # def coinToss(tosses=100):
-#     # randInt(tosses, 0, 1)
+# EOF
